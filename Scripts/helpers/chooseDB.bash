f8322509882f7bd175355a089e33a7cdd2233f08
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2023-12-18 00:25:47 +0300

    chooseDB Is Running

--- Scripts/helpers/chooseDB.bash
+++ Scripts/helpers/chooseDB.bash
@@ -1,21 +1,43 @@
 #!/bin/bash
+
+#params $1 operation number according to order
+operation=$1
+
 #get dbs
-cd ../../Databases
+cd /OS1-Project/Databases/
+
 #filter dbs
 user=$(whoami)
 list=( )
 for folder in *
 do
     if [ -d $folder ]; then
-        if groups $user | grep -q $folder; then
-        	echo $folder
-        fi
+        list+=($folder)
     fi
 done
+
 #view db with numbers
+i=0
+j=0
+for elm in list
+do
+  ((j=j+1))
+  echo "$j - ${list[i]}"
+  ((i=i+1))
+done
 
 #let user choose
 echo 'Choose A DB:'
-read dbName
+read dbNum
+
 #return the choosed table name
-ls | nl | grep $tableName | cut -f2-
+i=0
+j=0
+for elm in list
+do
+  ((j=j+1))
+  if [ $j -eq $dbNum ]; then
+  	echo ${list[i]}
+  fi
+  ((i=i+1))
+done
--- Scripts/helpers/choose_table.bash
+++ Scripts/helpers/choose_table.bash
@@ -1,12 +1,17 @@
 #!/bin/bash
+
 #get db name
 dbName=$1
+
 #get tables
-cd ../../Databases/$dbName
+cd /OS1-Project/Databases/$dbName
+
 #view tables with numbers
 ls | nl
+
 #let user choose
 echo 'Choose A Table:'
 read tableName
+
 #return the choosed table name
 ls | nl | grep $tableName | cut -f2-
